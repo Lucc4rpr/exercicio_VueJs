4d3867d2451474fa3b7f34ca898d75c8975bf155
Lucca <lucca07102002@gmail.com>
2024-08-27 17:09:01 -0300

    Exercicio calculadora com vueJs

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,67 +1,102 @@
-<script setup>
-    import { reactive, computed } from 'vue';
-    import Cabecalho from './components/Cabecalho.vue';
-    import Formulario from './components/Formulario.vue';
-    import ListaDeTarefas from './components/ListaDeTarefas.vue';
-
-
-    const estado = reactive( {
-        filtro: 'todas',
-        tarefaTemp: '',
-        tarefas: [
-            {
-                titulo: 'Estudar ES6',
-                finalizada: false,
-            },
-            {
-                titulo: 'Estudar SASS',
-                finalizada: false,
-            },
-            {
-                titulo: 'Ir para a academia',
-                finalizada: true,
-            }
-        ]
-    })
-
-    const getTarefasPendentes = () => {
-        return estado.tarefas.filter(tarefa => !tarefa.finalizada)
-    }
-
-    const getTarefasFinalizadas = () => {
-        return estado.tarefas.filter(tarefa => tarefa.finalizada)
-    }
-
-    const getTarefasFiltradas = () => {
-        const {filtro} = estado;
-
-        switch (filtro) {
-            case 'pendentes':
-                return getTarefasPendentes();
-        
-            case 'finalizadas':
-                return getTarefasFinalizadas();
-
-            default: 
-                return estado.tarefas;
-        }
-    }
-
-    const cadastraTarefa = () => {
-        const tarefaNova = {
-            titulo: estado.tarefaTemp,
-            finalizada: false, 
-        }
-        estado.tarefas.push(tarefaNova);
-        estado.tarefaTemp = '';
-    }
-</script>
-
 <template>
-    <div class="container">
-        <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
-        <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
-        <ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
+<div class="container">
+
+    
+    <div id="app">
+      <h1>Calculadora</h1>
+      <div class="calculator">
+        <div class="form-group">
+          <label for="num1">Digite o Primeiro Valor</label>
+          <input id="num1" type="number" v-model.number="num1" />
+        </div>
+        
+        <div class="form-group">
+          <label for="num2">Digite o Segundo Valor</label>
+          <input id="num2" type="number" v-model.number="num2" />
+        </div>
+        
+        <div class="form-group">
+          <label for="operation">Operação:</label>
+          <select id="operation" v-model="operation">
+            <option value="+">Soma</option>
+            <option value="-">Subtração</option>
+            <option value="*">Multiplicação</option>
+            <option value="/">Divisão</option>
+          </select>
+        </div>
+  
+        <div class="result">
+          <h2>Resultado: {{ resultado }}</h2>
+        </div>
+      </div>
     </div>
-</template>
+</div>
+  </template>
+  
+  <script setup>
+  import { computed, ref } from 'vue';
+  
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const operation = ref('+');
+  
+  const resultado = computed(() => {
+    switch (operation.value) {
+      case '+':
+        return num1.value + num2.value;
+      case '-':
+        return num1.value - num2.value;
+      case '*':
+        return num1.value * num2.value;
+      case '/':
+        return num2.value !== 0 ? num1.value / num2.value : 'Divisão por zero';
+      default:
+        return 0;
+    }
+  });
+  </script>
+  
+  <style scoped>
+
+  .container {
+    max-width: 600px;
+    height: 100vh;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .calculator {
+    max-width: 100%;
+    margin:  auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  }
+  
+  .form-group {
+    margin-bottom: 10px;
+  }
+  
+  input,
+  select {
+    width: 100%;
+    padding: 8px;
+    margin-top: 4px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+  
+  .result {
+    margin-top: 20px;
+    text-align: center;
+  }
+  
+  h2 {
+    color: #333;
+  }
+  </style>
+  
 
